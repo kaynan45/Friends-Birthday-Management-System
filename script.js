3f4a9ce348b932a3b2272af75ba75c6f00490b7e
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-03-07 13:30:27 -0300

    Implement the empty input name validation

--- script.js
+++ script.js
@@ -5,25 +5,30 @@ const myForm = document.querySelector(".js-form");
 
 //This /\d/ is a regular expression that checks if the input contains any digit (number).
 //The .test() method returns a Boolean value that indicates whether or not a pattern exists in a searched string. In this case, if the string contains a number, it'll be true, otherwise, false.
-nameInput.addEventListener('input', () => {
+nameInput.addEventListener("input", () => {
 
-    if(/\d/.test(nameInput.value)) {
-        nameInput.setCustomValidity("Meu querido, colocar o nome usando números? Deixa isso pros joguinhos");
-    } else {
-        nameInput.setCustomValidity('');
-    }
-})
+  if (nameInput.value.trim().length === 0) {
+    nameInput.setCustomValidity("E o nome queridao?");
+  } else if (/\d/.test(nameInput.value)) {
+    nameInput.setCustomValidity(
+      "Meu querido, colocar o nome usando números? Deixa isso pros joguinhos"
+    );
+  } 
+  else {
+    nameInput.setCustomValidity("");
+  }
+});
 
 const personData = [];
 
 function printOnConsole() {
-    myForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        personData.push({name: nameInput.value, birth: birthInput.value});
-        console.log(personData);
-        nameInput.value = '';
-        birthInput.value = '';
-    });
-};
+  myForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    personData.push({ name: nameInput.value, birth: birthInput.value });
+    console.log(personData);
+    nameInput.value = "";
+    birthInput.value = "";
+  });
+}
 
 printOnConsole();
