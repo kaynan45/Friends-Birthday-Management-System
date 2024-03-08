5c1b6c606286579d722c02f264f70eae1bc2f07b
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-03-08 10:42:22 -0300

    updating validations

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const nameInput = document.querySelector(".js-name-field");
 const birthInput = document.querySelector(".js-birth-field");
 const saveButton = document.querySelector(".js-save-button");
 const myForm = document.querySelector(".js-form");
-
-//This /\d/ is a regular expression that checks if the input contains any digit (number).
-//The .test() method returns a Boolean value that indicates whether or not a pattern exists in a searched string. In this case, if the string contains a number, it'll be true, otherwise, false.
+/*
+ *This /\d/ is a regular expression that checks if the input contains any digit (number).
+ *The .test() method returns a Boolean value that indicates whether or not a pattern exists in a searched string.
+ *In this case, if the string contains a number, it'll be true, otherwise, false.
+ */
 nameInput.addEventListener("input", () => {
-
   if (nameInput.value.trim().length === 0) {
     nameInput.setCustomValidity("E o nome queridao?");
   } else if (/\d/.test(nameInput.value)) {
     nameInput.setCustomValidity(
       "Meu querido, colocar o nome usando números? Deixa isso pros joguinhos"
     );
-  } 
-  else {
+  } else {
     nameInput.setCustomValidity("");
   }
 });
