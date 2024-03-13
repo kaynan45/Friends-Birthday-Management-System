eb24d8f0b038dc5b8cf6e446d59a3f73d88abeab
Lorenzo Kaynan Xavier <lorenzokaynanxavier@gmail.com>
2024-03-13 13:15:53 -0300

    create the THML and CSS

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const addFriendButton = document.getElementById("insert-friends-button");
+const closeModal = document.querySelector(".close-modal");
 
+addFriendButton.addEventListener('click', () => {
+    document.querySelector(".modal").classList.add("active");
+});
+
+closeModal.addEventListener('click', () => {
+    document.querySelector(".modal").classList.remove("active");
+});
